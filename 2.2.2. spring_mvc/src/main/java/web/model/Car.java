325b7eda556f3963979ee11681eccb654a51dddf
pschenizzza <guitar57@yandex.ru>
2021-07-13 13:58:23 +0700

    Initial commit

---
+++ 2.2.2. spring_mvc/src/main/java/web/model/Car.java
@@ -0,0 +1,37 @@
+package web.model;
+
+public class Car {
+    private long id;
+    private String model;
+    private int horsePower;
+
+    public Car() {}
+
+    public Car(long id, String model, int horsePower) {
+        this.id = id;
+        this.model = model;
+        this.horsePower = horsePower;
+    }
+
+    public long getId() { return id; }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+}
---
+++ 2.2.2. spring_mvc/src/main/java/web/service/CarService.java
@@ -0,0 +1,20 @@
+package web.service;
+
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarService {
+
+    public List<Car> getAllCars() {
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car(1, "1970 Dodge Charger", 900));
+        cars.add(new Car(2, "2014 W Motors Lykan HyperSport", 800));
+        cars.add(new Car(3, "1969 Dodge Charger Daytona", 425));
+        cars.add(new Car(4, "1965 Chevrolet Corvette Stingray", 360));
+        cars.add(new Car(5, "2021 Toyota GR Supra", 382));
+        return cars;
+    }
+}
+
